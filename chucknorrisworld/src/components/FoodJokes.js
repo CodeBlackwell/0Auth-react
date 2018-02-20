57fabd901a5be68f273c78b1e9c9b07aabb25111
LeChristopher Blackwell <codeblackwell@gmail.com>
2018-02-20 09:45:32 -0700

    added all components

---
+++ chucknorrisworld/src/components/FoodJokes.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+import Nav from './Nav';
+import { getFoodData } from './utils/chucknorris-api';
+
+class FoodJokes extends Component {
+    constructor() {
+        super()
+        this.state = { jokes: [] };
+    }
+
+    getFoodJokes() {
+        getFoodData().then(jokes => {
+            this.setState({ jokes })
+        })
+    }
+
+    componentDidMount() {
+        this.getFoodJokes();
+    }
+
+    render() {
+        const { jokes } = this.state;
+
+        return (
+            <div>
+                <Nav />
+                <h3 className="text-center">Chuck Norris Food Jokes</h3>
+                <hr/>
+                { joke.map((joke, index) => { return (
+                    <div className="col-sm-6" key={index}>
+                        <div className="panel panel-primary">
+                            <div className="panel-heading">
+                                <h3 className="panel-title"> <span className="btn">{ joke.id }</span></h3>
+                            </div>
+                            <div className="panel-body">
+                                <p>{ joke.joke }</p>
+                            </div>
+                        </div>
+                    </div>
+                )})}
+            </div>
+        )
+    }
+}
+
+export default FoodJokes;
